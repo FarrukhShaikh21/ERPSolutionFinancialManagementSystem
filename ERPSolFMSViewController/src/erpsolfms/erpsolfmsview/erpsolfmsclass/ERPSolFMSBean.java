c3e2e4a394d686e914499adcc52d0a4223350b22
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2021-11-24 20:54:44 +0500

    INTIAL COMMIT
    INTIAL COMMIT

---
+++ ERPSolFMSViewController/src/erpsolfms/erpsolfmsview/erpsolfmsclass/ERPSolFMSBean.java
@@ -0,0 +1,55 @@
+package erpsolfms.erpsolfmsview.erpsolfmsclass;
+
+import oracle.adf.share.ADFContext;
+
+public class ERPSolFMSBean {
+    public ERPSolFMSBean() {
+        super();
+    }
+
+    String ERPSolStrUserCode;
+    String ERPSolStrUserLocationCode;
+    String ERPSolStrUserRegionCode;
+    String ERPSolStrUserStoreCode;
+
+    public void doSetERPSolFMSSessionGlobals() {
+        System.out.println("glob user code"+getERPSolStrUserCode());
+        ADFContext.getCurrent().getPageFlowScope().put("GLOB_USER_CODE", getERPSolStrUserCode());
+        ADFContext.getCurrent().getPageFlowScope().put("GLOB_USER_REGION", getERPSolStrUserRegionCode());
+        ADFContext.getCurrent().getPageFlowScope().put("GLOB_USER_LOCATION", getERPSolStrUserLocationCode());
+        ADFContext.getCurrent().getPageFlowScope().put("GLOB_USER_STORE", getERPSolStrUserStoreCode());
+        ADFContext.getCurrent().getPageFlowScope().put("GLOB_COMPANY_CODE", 2);
+    }
+
+    public void setERPSolStrUserCode(String ERPSolStrUserCode) {
+        this.ERPSolStrUserCode = ERPSolStrUserCode;
+    }
+
+    public String getERPSolStrUserCode() {
+        return ERPSolStrUserCode;
+    }
+
+    public void setERPSolStrUserLocationCode(String ERPSolStrUserLocationCode) {
+        this.ERPSolStrUserLocationCode = ERPSolStrUserLocationCode;
+    }
+
+    public String getERPSolStrUserLocationCode() {
+        return ERPSolStrUserLocationCode;
+    }
+
+    public void setERPSolStrUserRegionCode(String ERPSolStrUserRegionCode) {
+        this.ERPSolStrUserRegionCode = ERPSolStrUserRegionCode;
+    }
+
+    public String getERPSolStrUserRegionCode() {
+        return ERPSolStrUserRegionCode;
+    }
+
+    public void setERPSolStrUserStoreCode(String ERPSolStrUserStoreCode) {
+        this.ERPSolStrUserStoreCode = ERPSolStrUserStoreCode;
+    }
+
+    public String getERPSolStrUserStoreCode() {
+        return ERPSolStrUserStoreCode;
+    }
+}
